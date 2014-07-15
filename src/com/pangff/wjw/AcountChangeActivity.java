a314a7fe6af801c57c459ab1917dd3e8e2e9fcd3
pangxinlong <670116586@qq.com>
2014-07-15 09:25:35 +0800

    更新了会员转帐.

---
+++ src/com/pangff/wjw/AcountChangeActivity.java
@@ -0,0 +1,18 @@
+package com.pangff.wjw;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class AcountChangeActivity extends BaseActivity {
+
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_account_change);
+	}
+	public static void invotoacountchange(BaseActivity context){
+		Intent intent = new Intent();
+        intent.setClass(context, AcountChangeActivity.class);  
+        context.startActivity(intent);
+	}
+	
+}
